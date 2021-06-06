bf8cec61312ce8dabc6659f7a915d27cc7a872db
arthurmezaonik <arthur.mezaonik@gmail.com>
2021-06-06 17:28:50 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswear(){
+
+}
+
+function calculateCorrectAnswear(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswear(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
+
+function displayDivisionQuestion(){
+    
+}

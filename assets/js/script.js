4328645baf6e1c455759bdbd2e4b4a28d2a6b0aa
arthurmezaonik <arthur.mezaonik@gmail.com>
2021-06-07 20:17:07 +0100

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function(){
     for (let button of buttons){
         button.addEventListener('click', function(){
             if (this.getAttribute('data-type') === 'submit'){
-                alert('You clicked submit');
+                checkAnswear();
             } else{
                 let gameType = this.getAttribute('data-type');
                 runGame(gameType);
@@ -43,8 +43,23 @@ function runGame(gameType){
     }
 }
 
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswear(){
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculateAnswer = calculateCorrectAnswear();
+    let isCorrect = userAnswer === calculateAnswer[0];
+    console.log (isCorrect)
 
+    if (isCorrect){
+        alert("Hey! You got it right! :D");
+    } else{
+        alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculateAnswer[0]}`);
+    }
+
+    runGame(calculateAnswer[1]);
 }
 
 /**
@@ -55,19 +70,19 @@ function calculateCorrectAnswear(){
 
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById('operator').innetText;
+    let operator = document.getElementById('operator').innerText;
 
     if(operator === "+"){
-        return [operand1 + operand2, 'addition']
+        return [operand1 + operand2, 'addition'];
     } else if( operator === "-"){
-        return [operand1 - operand2, 'subtract']
+        return [operand1 - operand2, 'subtract'];
     } else if( operator === "x"){
-        return [operand1 * operand2, 'multiply']
+        return [operand1 * operand2, 'multiply'];
     } else if( operator === "%"){
-        return [operand1 / operand2, 'division']
+        return [operand1 / operand2, 'division'];
     } else{
-        alert(`Unimplemented operator ${operator}`)
-        throw `Unimplemented operator ${operator}. Aborting!`
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }
 
